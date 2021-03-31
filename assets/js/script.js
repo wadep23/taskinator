986a548ad93600812a14fa803661259a474189ef
wadep <wadepfacer@gmail.com>
2021-03-31 16:48:10 -0600

    added task type and innerhtml elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,25 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 // creating a function to duplicate the anonymous function in event listener.
 var createTaskHandler = function() {
     event.preventDefault();
+
+    var taskNameInput = document.querySelector("input[name = 'task-name']") .value;
+    var taskTypeInput = document.querySelector("select[name= 'task-type']").value;
     
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3> <span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+    
     tasksToDoEl.appendChild(listItemEl);
     
 }
 
-// Create a new task item using event listener for button click.
+// Create a new task item using event listener.
 formEl.addEventListener("submit", createTaskHandler);
 
