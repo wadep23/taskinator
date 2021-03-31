cbc5332d68832873af6f968df1f7103861d05e75
wadep <wadepfacer@gmail.com>
2021-03-31 16:00:06 -0600

    added a prevent default event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector('#task-form');
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // creating a function to duplicate the anonymous function in event listener.
 var createTaskHandler = function() {
+    event.preventDefault();
+    
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
+    
 }
 
 // Create a new task item using event listener for button click.
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
